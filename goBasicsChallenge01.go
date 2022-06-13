bb444a3f2f31b23fcb2ab127c114ab3044a7deb9
Bhaskar Kanukunta <bhaskarkanukunta@users.noreply.github.com>
2022-06-13 16:01:22 +0000

    go variables

---
+++ goBasicsChallenge01.go
@@ -0,0 +1,13 @@
+/* Alta3 GoLang training | Bhaskar
+   Print name, city and state */
+
+package main
+
+import "fmt"
+
+
+func main() {
+    fmt.Println("My name is: Bhaskar")    
+    fmt.Println("My city is: Minneapolis") 
+    fmt.Println("My name is: Minnesota")
+}
